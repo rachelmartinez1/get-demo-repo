85c8095b52317f67928b389e7e250bd1a278c95d
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 17:26:33 +0000

    Portfolio items/pageupdated

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -23,4 +23,16 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
   
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :body))
+       format.html { redirect_to portfolios_path, notice: 'Your record was successfully updated.' }
+       else
+        format.html { render :new }
+      end
+    end
+  end
+  
 end
