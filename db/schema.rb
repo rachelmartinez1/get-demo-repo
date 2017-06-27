e991ace7803c9c6f6ca96ecb0cdfb8e93e59ffc5
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 19:35:00 +0000

    Implemented concern for managing image data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626232847) do
+ActiveRecord::Schema.define(version: 20170627164916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 20170626232847) do
     t.datetime "updated_at",             null: false
     t.string   "slug"
     t.integer  "status",     default: 0
-    t.integer "topic_id"
+    t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
-    t.index ["topic_id"], name: "index_blogs_on_topic", using: :btree
+    t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170626232847) do
     t.integer  "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "badge"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -62,4 +63,5 @@ ActiveRecord::Schema.define(version: 20170626232847) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "topics"
 end
