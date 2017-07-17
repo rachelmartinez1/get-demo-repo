f4ca626c46bc143f236772aca1686c021beb69cc
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 03:01:38 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
    content_tag(:p, greeting, class: "source-greeting")
    end
   end
+  
+  def copyright_generator
+    RMartinezViewTool::Renderer.copyright 'Rachel Martinez', 'All rights reserved'
+  end
 end
