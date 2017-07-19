96a1048bbffacd22d47a3817d2529adc9eb2c0c6
Jordan Hudgens <jordan@devcamp.com>
2017-07-19 15:13:18 +0000

    Updated styles of Login/signup/forgot password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   before_action :set_copyright
 
-def set_copyright
-  @copyright = RMartinezViewTool::Renderer.copyright 'Rachel Martinez', 'All rights reserved'
+  def set_copyright
+    @copyright = RMartinezViewTool::Renderer.copyright 'Rachel Martinez', 'All rights reserved'
   end
 end
