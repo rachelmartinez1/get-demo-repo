02b8c80efa2acbc7715c5ccb5f33d183099ed7ea
Jordan Hudgens <jordan@devcamp.com>
2017-07-25 00:18:57 +0000

    Updated portfolio to include attr for position

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,25 @@
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
-     (link_to "Register", new_user_registration_path, class: style) +
-     " ".html_safe +
-     (link_to "Login", new_user_session_path, class: style)
-   else
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
+    else
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
-  
+
   def source_helper(layout_name)
-   if session[:source]
-   greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-   content_tag(:p, greeting, class: "source-greeting")
-   end
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
   end
-  
+
   def copyright_generator
     RMartinezViewTool::Renderer.copyright 'Rachel Martinez', 'All rights reserved'
-  end 
-  
+  end
+
   def nav_items
     [
       {
@@ -44,19 +44,19 @@ module ApplicationHelper
       },
     ]
   end
-  
-  
+
   def nav_helper style, tag_type
     nav_links = ''
-    
+
     nav_items.each do |item|
-      nav_links  << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
-  end
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
 
     nav_links.html_safe
   end
-  
+
   def active? path
     "active" if current_page? path
   end
+
 end
