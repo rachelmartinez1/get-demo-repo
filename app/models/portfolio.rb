8b99bf56dd0a6eb8453122de7ccf8a55f161fcd7
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 01:58:21 +0000

    Updated dfile upload forms and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,9 +5,6 @@ class Portfolio < ApplicationRecord
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   validates_presence_of :title, :body
-
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
   
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -21,11 +18,4 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-  
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
