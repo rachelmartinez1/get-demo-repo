bfe68b591541644e73bcd86b1dab77b352408de3
Jordan Hudgens <jordan@devcamp.com>
2017-07-29 01:06:54 +0000

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,17 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert_generator alert
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
+  end
+
+
+
+
+
+
 end
