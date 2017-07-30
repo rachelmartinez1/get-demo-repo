3aaf559a760a5eb5d82e4e2cc285253e4f80bd54
Jordan Hudgens <jordan@devcamp.com>
2017-07-30 23:42:57 +0000

    Created a comment broadcast configured the websocket route and updated the comment model with callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :blog
   
   validates :content, presence: true, length: { minimum: 3, maximum: 100 }
+
+  after_create_commit { CommentBroadcastJob.perform_later(self) }
+
 end
