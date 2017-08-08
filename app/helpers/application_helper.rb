c84af56589cbc0d8d278133cf5c61c9f4200491c
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 14:31:47 +0000

    Helping student

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,10 @@ module ApplicationHelper
     end
   end
 
-  def source_helper(layout_name)
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{link_to 'contact me', contact_path} if you'd like to work together."
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end
 
@@ -70,10 +70,6 @@ module ApplicationHelper
   def alert_generator msg
     js add_gritter(msg, title: "Rachel Martinez Portfolio", sticky: false)
   end
-
-
-
-
 
 
 end
